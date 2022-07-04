36589d2595bed84dedbfc75920f7c9c05ecf71f2
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-04 13:25:37 -0400

    added webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
 const path = require("path");
 
+// bring in webpack
+const webpack = require("webpack");
 // basic configuration: entry, output, mode
 
-module.exports = {};
+module.exports = {
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.bundle.js",
+  },
+  plugins: [
+    // adding plugin, like JQUERY
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
+  mode: "development",
+};
